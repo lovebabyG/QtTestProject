b708547d9b6db3a7e16b39c810839e8e0ce279f1
unknown <ZYin@HIULP762.ad.harman.com>
2016-01-14 11:09:07 +0100

    add main.cpp

---
+++ MyQtTestProject/main.cpp
@@ -0,0 +1,10 @@
+
+#include <QtWidgets>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QPushButton button("Hello world");
+    button.show();
+    return app.exec();
+}
